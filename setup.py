54603c601589d5d26d08e2a85f1f8967e6ecb1ca
veraposeidon <veraposeidon@gmail.com>
2021-11-18 13:12:48 +0800

    MOD: 修改package说明

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 """setup.py for install this package"""
 from setuptools import setup, find_packages
 
+with open('README.md') as readme_f:
+    long_description = readme_f.read()
+
 setup(
     name='labelme2datasets',
-    version='0.1',
+    version='0.3',
     description='python scripts to convert labelme-generated-jsons to voc/coco style datasets.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/veraposeidon/labelme2Datasets",
     author='veraposeidon',
     packages=find_packages(include=['labelme2datasets', 'labelme2datasets.*']),
     install_requires=[
@@ -20,4 +26,9 @@ setup(
             'labelme_bbox_json2voc = labelme2datasets.labelme_bbox_json2voc:main'
         ]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
