6cd0083ffc90b0b6e7fc53c8c7eb69a8cde20bee
hi.xiaohai <veraposeidon@gmail.com>
2024-05-12 12:04:15 +0800

    fix: limit pylint python version to 3.9

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         ]
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
