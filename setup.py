478bb88992ad9bec23f3f073075b02bc613417e8
hi.xiaohai <veraposeidon@gmail.com>
2024-05-12 12:25:54 +0800

    ci: limit to py310

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         ]
     },
     classifiers=[
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
